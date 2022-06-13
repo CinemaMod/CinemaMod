0ec7d5987a7958d1c9b4e0d4adc462d13e91c5dc
ds58 <30220598+ds58@users.noreply.github.com>
2022-06-13 12:27:31 -0500

    Change remote urls

--- src/main/java/com/cinemamod/downloader/Resource.java
+++ src/main/java/com/cinemamod/downloader/Resource.java
@@ -3,8 +3,8 @@ package com.cinemamod.downloader;
 public final class Resource {
 
     public static final String CINEMAMOD_VERSIONS_URL = "https://cinemamod-libraries.ewr1.vultrobjects.com/versions.txt";
-    public static final String CINEMAMOD_JCEF_URL_FORMAT = "https://ewr1.vultrobjects.com/cinemamod-libraries/jcef/%s/%s";
-    public static final String CINEMAMOD_JCEF_PATCHES_URL_FORMAT = "https://ewr1.vultrobjects.com/cinemamod-libraries/jcef-patches/%s/%s";
+    public static final String CINEMAMOD_JCEF_URL_FORMAT = "https://cinemamod-libraries.ewr1.vultrobjects.com/jcef/%s/%s";
+    public static final String CINEMAMOD_JCEF_PATCHES_URL_FORMAT = "https://cinemamod-libraries.ewr1.vultrobjects.com/jcef-patches/%s/%s";
 
     public static String getJcefUrl(String cefBranch, String platform) {
         return CINEMAMOD_JCEF_URL_FORMAT.formatted(cefBranch, platform);
