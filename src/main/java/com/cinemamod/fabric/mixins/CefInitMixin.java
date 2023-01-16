b876d6227a10d7dc36c198abc5824cc68ec8a05b
ds58 <30220598+ds58@users.noreply.github.com>
2023-01-16 14:56:51 -0600

    Start per-platform mega jars

--- src/main/java/com/cinemamod/fabric/mixins/CefInitMixin.java
+++ src/main/java/com/cinemamod/fabric/mixins/CefInitMixin.java
@@ -1,57 +1,59 @@
 package com.cinemamod.fabric.mixins;
 
-import com.cinemamod.downloader.CinemaModDownloader;
 import com.cinemamod.fabric.CinemaMod;
 import com.cinemamod.fabric.cef.CefUtil;
+import com.cinemamod.fabric.cef.Platform;
 import net.minecraft.client.main.Main;
-import org.cef.OS;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * A mixin is used here to load JCEF at the earliest point in the MC bootstrap process
  * See: net.minecraft.client.main.Main
  * This reduces issues with CEF initialization
- * Due to AWT issues on MacOS, we cannot initialize CEF here
+ * Due to AWT issues on macOS, we cannot initialize CEF here
  */
 @Mixin(Main.class)
 public class CefInitMixin {
 
-    private static void setupLibraryPath() {
-        Path minecraftPath = Paths.get("");
-        Path modsPath = minecraftPath.resolve("mods");
-        Path cinemaModLibrariesPath = modsPath.resolve("cinemamod-libraries");
-
-        if (Files.notExists(cinemaModLibrariesPath)) {
-            try {
-                Files.createDirectory(cinemaModLibrariesPath);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    private static void setupLibraryPath(Platform platform) throws IOException {
+        // Check for development environment
+        // i.e. cinemamod-repo/build/cef/<platform>
+        File cefPlatformDir = new File("../build/cef/" + platform.getNormalizedName());
+        if (cefPlatformDir.exists()) {
+            System.setProperty("cinemamod.libraries.path", cefPlatformDir.getCanonicalPath());
+            return;
         }
 
-        System.setProperty("cinemamod.libraries.path", cinemaModLibrariesPath.toAbsolutePath().toString());
+        // Check for .minecraft/mods/cinemamod-libraries directory
+        File cinemaModLibrariesDir = new File("mods/cinemamod-libraries");
+        if (!cinemaModLibrariesDir.exists()) {
+            cinemaModLibrariesDir.mkdirs();
+        }
+        System.setProperty("cinemamod.libraries.path", cinemaModLibrariesDir.getCanonicalPath());
     }
 
     @Inject(at = @At("HEAD"), method = "main ([Ljava/lang/String;)V", remap = false)
     private static void cefInit(CallbackInfo info) {
-        setupLibraryPath();
+        Platform platform = Platform.getPlatform();
 
-        CinemaModDownloader.main(new String[]{});
+        try {
+            setupLibraryPath(platform);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         // TODO: Move to org.cef.CefApp
-        if (OS.isLinux()) {
+        if (platform.isLinux()) {
             System.loadLibrary("jawt");
         }
 
-        if (OS.isWindows() || OS.isLinux()) {
+        if (platform.isLinux() || platform.isWindows()) {
             if (CefUtil.init()) {
                 CinemaMod.LOGGER.info("Chromium Embedded Framework initialized");
             } else {
