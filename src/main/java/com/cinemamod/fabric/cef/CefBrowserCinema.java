b574bad88f9c57e9744bd330612f3df504d84be2
ds58 <30220598+ds58@users.noreply.github.com>
2023-01-21 21:24:56 -0600

    More rendering code cleanup

--- src/main/java/com/cinemamod/fabric/cef/CefBrowserCinema.java
+++ src/main/java/com/cinemamod/fabric/cef/CefBrowserCinema.java
@@ -5,7 +5,6 @@ import org.cef.CefClient;
 import org.cef.browser.CefBrowser;
 import org.cef.browser.CefBrowserOsr;
 import org.cef.browser.CefRequestContext;
-import org.lwjgl.BufferUtils;
 
 import java.awt.*;
 import java.nio.ByteBuffer;
@@ -13,60 +12,15 @@ import java.nio.ByteBuffer;
 public class CefBrowserCinema extends CefBrowserOsr {
 
     public final CefBrowserCinemaRenderer renderer = new CefBrowserCinemaRenderer(true);
-    private final CefImageData imageData = new CefImageData();
-
-    private class CefBrowserCinemaUpdateThread extends Thread {
-        @Override
-        public void run() {
-            while (true) {
-                synchronized (imageData) {
-                    if (imageData.shouldClose) {
-                        return;
-                    }
-
-                    if (imageData.hasFrame) {
-                        MinecraftClient.getInstance().submit(() -> renderer.onPaint(imageData.dirtyRects, imageData.buffer, imageData.width, imageData.height, imageData.fullReRender));
-                        imageData.hasFrame = false;
-                        imageData.fullReRender = false;
-                    }
-                }
-
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-    }
 
     public CefBrowserCinema(CefClient client, String url, boolean transparent, CefRequestContext context) {
         super(client, url, transparent, context);
-        new CefBrowserCinemaUpdateThread().start();
+        MinecraftClient.getInstance().submit(renderer::initialize);
     }
 
     @Override
     public void onPaint(CefBrowser browser, boolean popup, Rectangle[] dirtyRects, ByteBuffer buffer, int width, int height) {
-        final int size = (width * height) << 2;
-
-        synchronized (imageData) {
-            if (imageData.hasFrame) // The previous frame was not uploaded to GL texture, so we skip it and render this on instead
-                imageData.fullReRender = true;
-
-            if (imageData.buffer == null || size != imageData.buffer.capacity()) // This only happens when the browser gets resized
-                imageData.buffer = BufferUtils.createByteBuffer(size);
-
-            imageData.buffer.position(0);
-            imageData.buffer.limit(buffer.limit());
-            buffer.position(0);
-            imageData.buffer.put(buffer);
-            imageData.buffer.position(0);
-
-            imageData.width = width;
-            imageData.height = height;
-            imageData.dirtyRects = dirtyRects;
-            imageData.hasFrame = true;
-        }
+        renderer.onPaint(buffer, width, height);
     }
 
     public void resize(int width, int height) {
@@ -77,7 +31,6 @@ public class CefBrowserCinema extends CefBrowserOsr {
     public void close() {
         renderer.cleanup();
         super.close(true);
-        imageData.shouldClose = true;
     }
 
 }
--- src/main/java/com/cinemamod/fabric/cef/CefBrowserCinemaRenderer.java
+++ src/main/java/com/cinemamod/fabric/cef/CefBrowserCinemaRenderer.java
@@ -2,7 +2,6 @@ package com.cinemamod.fabric.cef;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 
-import java.awt.*;
 import java.nio.ByteBuffer;
 
 import static org.lwjgl.opengl.GL11.*;
@@ -12,66 +11,40 @@ import static org.lwjgl.opengl.GL12.GL_UNSIGNED_INT_8_8_8_8_REV;
 public class CefBrowserCinemaRenderer {
 
     private final boolean transparent;
-    private int[] textureID = new int[1];
-    private int width = 0;
-    private int height = 0;
+    private final int[] textureID = new int[1];
 
     protected CefBrowserCinemaRenderer(boolean transparent) {
         this.transparent = transparent;
-        initialize();
     }
 
-    public int getTextureID() {
-        return textureID[0];
-    }
-
-    protected void initialize() {
+    public void initialize() {
         RenderSystem.enableTexture();
         textureID[0] = glGenTextures();
         RenderSystem.bindTexture(textureID[0]);
         RenderSystem.texParameter(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         RenderSystem.texParameter(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         RenderSystem.bindTexture(0);
+        RenderSystem.disableTexture();
+    }
+
+    public int getTextureID() {
+        return textureID[0];
     }
 
     protected void cleanup() {
-        if (textureID[0] != 0) {
+        if (textureID[0] != 0)
             glDeleteTextures(textureID[0]);
-        }
     }
 
-    protected void onPaint(Rectangle[] dirtyRects, ByteBuffer buffer, int width, int height, boolean rerender) {
-        if (transparent) {
-            // Enable alpha blending
-            RenderSystem.enableBlend();
-        }
-
-        // Enable 2D textures
+    protected void onPaint(ByteBuffer buffer, int width, int height) {
+        if (transparent) RenderSystem.enableBlend();
         RenderSystem.enableTexture();
-
         RenderSystem.bindTexture(textureID[0]);
-
-        int lastWidth = this.width;
-        int lastHeight = this.height;
-
-        this.width = width;
-        this.height = height;
-
-        RenderSystem.pixelStore(GL_UNPACK_ROW_LENGTH, this.width);
-
-        if (rerender || lastWidth != this.width || lastHeight != this.height) {
-            RenderSystem.pixelStore(GL_UNPACK_SKIP_PIXELS, 0);
-            RenderSystem.pixelStore(GL_UNPACK_SKIP_ROWS, 0);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this.width, this.height, 0,
-                    GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, buffer);
-        } else {
-            for (Rectangle rect : dirtyRects) {
-                RenderSystem.pixelStore(GL_UNPACK_SKIP_PIXELS, rect.x);
-                RenderSystem.pixelStore(GL_UNPACK_SKIP_ROWS, rect.y);
-                glTexSubImage2D(GL_TEXTURE_2D, 0, rect.x, rect.y, rect.width,
-                        rect.height, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, buffer);
-            }
-        }
+        RenderSystem.pixelStore(GL_UNPACK_ROW_LENGTH, width);
+        RenderSystem.pixelStore(GL_UNPACK_SKIP_PIXELS, 0);
+        RenderSystem.pixelStore(GL_UNPACK_SKIP_ROWS, 0);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
+                GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, buffer);
     }
 
 }
