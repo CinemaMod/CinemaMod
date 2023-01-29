7f1a34d67c5bf35b4f92a4cb2d209185efe6faf7
ds58 <30220598+ds58@users.noreply.github.com>
2023-01-29 15:45:41 -0600

    Remove build_test.sh

--- build_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-
-mvn compile
-mvn install
-sleep 0.5
-cp -f target/CinemaMod.jar ../CinemaTestServer/plugins/CinemaMod.jar
-tmux send-keys -t CinemaTestServer.0 "plugman reload CinemaMod" ENTER
