1bae2cea8c17a98a6fa197209246782e615f939e
ds58 <30220598+ds58@users.noreply.github.com>
2025-01-13 20:49:55 -0600

    Fix building

--- bukkit/src/main/java/com/cinemamod/bukkit/theater/screen/Screen.java
+++ bukkit/src/main/java/com/cinemamod/bukkit/theater/screen/Screen.java
@@ -2,7 +2,7 @@ package com.cinemamod.bukkit.theater.screen;
 
 import com.cinemamod.bukkit.buffer.PacketByteBufReimpl;
 import com.cinemamod.bukkit.buffer.PacketByteBufSerializable;
-import org.apache.commons.lang.NotImplementedException;
+import org.apache.commons.lang3.NotImplementedException;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
